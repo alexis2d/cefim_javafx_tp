c4edce879577bfd241444e7749ab537df9e1a148
Alexis Duquenet <alexisduquenet@gmail.com>
2023-06-27 15:16:29 +0200

    Sliders and rectangle color change events OK

--- src/main/java/alexis/cefim_javafx_tp/controller/MainController.java
+++ src/main/java/alexis/cefim_javafx_tp/controller/MainController.java
@@ -2,22 +2,53 @@ package alexis.cefim_javafx_tp.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class MainController {
 
     @FXML
+    private Slider redRange;
+    @FXML
+    private Slider greenRange;
+    @FXML
+    private Slider blueRange;
+
+    @FXML
+    private TextField redCode;
+    @FXML
+    private TextField greenCode;
+    @FXML
+    private TextField blueCode;
+
+    @FXML
+    private Rectangle colorRectangle;
+
+    @FXML
     protected void onRedRangeMouseRealeased() {
-        System.out.println("red");
+        redCode.clear();
+        redCode.appendText(String.valueOf(Math.round(redRange.getValue())));
+        updateColorRectangle();
     }
 
     @FXML
     protected void onGreenRangeMouseRealeased() {
-        System.out.println("green");
+        greenCode.clear();
+        greenCode.appendText(String.valueOf(Math.round(greenRange.getValue())));
+        updateColorRectangle();
     }
 
     @FXML
     protected void onBlueRangeMouseRealeased() {
-        System.out.println("blue");
+        blueCode.clear();
+        blueCode.appendText(String.valueOf(Math.round(blueRange.getValue())));
+        updateColorRectangle();
+    }
+
+    private void updateColorRectangle() {
+        colorRectangle.setFill(Color.rgb(Integer.parseInt(redCode.getText()),Integer.parseInt(greenCode.getText()),Integer.parseInt(blueCode.getText())));
     }
 
 }
